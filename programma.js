00dda0f8130fb67325bb911e8eeb52439e17f2f3
nicofiorescuola <nicolo.fiore@iismontalegenova.it>
2024-11-19 10:49:38 +0100

    elementi

--- programma.js
+++ programma.js
@@ -1,28 +1,35 @@
 
 
-let contenitore = document.getElementById('contenitore');
-contenitore.style.borderStyle = 'solid';
+for (const element of elementi) {
+    let contenitore = document.getElementById('contenitore');
+contenitore.style.position = 'relative';
 
 let elemento = document.createElement('div');
-elemento.style.borderStyle = 'solid'
-elemento.style.width = '70px'
-elemento.style.textAlign = 'center'
-elemento.style.position = 'absolute'
-elemento.style.top = '500px';
-elemento.style.left = '500px';
+elemento.style.borderStyle = 'solid';
+elemento.style.width = '100px';
+elemento.style.height='120px';
+elemento.style.textAlign = 'center';
+elemento.style.position ='absolute';
+elemento.style.left = 100 * (element.xpos - 1) + 'px';
+elemento.style.top = 120 * (element.ypos - 1 ) + 'px';
+elemento.style.backgroundColor = '#' + element['cpk-hex'];
+
 
 let numeroAtomico = document.createElement('p');
-numeroAtomico.innerText = 1;
+numeroAtomico.innerText =element.number;
 elemento.appendChild(numeroAtomico);
+numeroAtomico.style.textAlign = left;
 
 let simbolo = document.createElement('a');
-simbolo.innerText = 'H';
-simbolo.href = 'https://it.wikipedia.org/wiki/Idrogeno'
-simbolo.style.fontSize = '25px'
+simbolo.innerText =element.symbol;
+simbolo.href = element.source;
+simbolo.style.fontSize = '25px';
 elemento.appendChild(simbolo);
+simbolo.style.textAlign = Blob;
 
 let nome = document.createElement('p');
-nome.innerText = 'idrogeno';
+nome.innerText = element.name;
 elemento.appendChild(nome);
 
 contenitore.appendChild(elemento);
+}
